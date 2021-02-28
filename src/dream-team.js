d68d4618994fd613ad4dba1faa103f1f09c2fa53
HelenKolesnikovich <solnechnaya_88@list.ru>
2021-02-28 15:27:53 +0300

    feat: add 'Dream-Team' task solution

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,25 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if(!Array.isArray(members)){
+	  return false;
+  }
+
+  let dreamTeamName = '';
+  members.forEach(function(element){
+	if(typeof element == 'string')
+	{
+		dreamTeamName += element.trimStart().charAt(0);
+	}
+  });
+
+  if(dreamTeamName.length == 0){
+	  return false;
+  }  
+  else if(dreamTeamName.length > 1){
+    return dreamTeamName.toUpperCase().split('').sort().join('');
+  }
+  else{
+	  return dreamTeamName.toUpperCase();
+  }
 };
