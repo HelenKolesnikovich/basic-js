da45ca8c2ce7a4e2ad92c0fd0e0adac9aa864fc7
HelenKolesnikovich <solnechnaya_88@list.ru>
2021-03-11 12:54:05 +0300

    feat: add a solution for the Extended-Repeater task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,35 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function repeater(str, options) {
+  const mainSubstring = typeof arguments[0] === 'string' ? arguments[0] : arguments[0] + '';
+  const additionalString = String(arguments[1].addition) === 'undefined' ? '' : String(arguments[1].addition);
+  const mainSeparator = options.separator ? options.separator + '' : '+';
+  const additionSeparator = options.additionSeparator ? options.additionSeparator + '' : '|'; 
+
+  let additionPart = options.additionRepeatTimes ? getAdditioanlPart(additionalString, additionSeparator, options.additionRepeatTimes) : additionalString;
+
+  let resultSubstring = mainSubstring + additionPart;
+
+  let resultString = options.repeatTimes ? getResultString(resultSubstring, mainSeparator, options.repeatTimes) : resultSubstring;
+
+  return resultString;
+}
+
+function getAdditioanlPart(string, separator, repeatTimes) {
+  let additionPart = string;
+  for(let i = 1; i < repeatTimes; i++)
+  {
+      additionPart += separator + string;
+  }
+  return additionPart;
+}
+
+function getResultString(substring, separator, repeatTimes) {
+  let resultString = substring;
+  for(let i = 1; i < repeatTimes; i++)
+  {
+      resultString += separator + substring; 
+  }
+  return resultString;
+}
   
