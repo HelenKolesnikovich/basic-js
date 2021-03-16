51e3183e2eea9f3483cad08d7913cb0957534e75
HelenKolesnikovich <solnechnaya_88@list.ru>
2021-03-16 23:38:53 +0300

    feat: implement solution for 'Recursive Depth' task

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,54 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth(arr) {
+    if(!Array.isArray(arr)) {
+      return 'Error!';
+    }
+
+    var subArraysLenghts = [];
+	  if(Array.isArray(arr)){
+		  if (arr.length>0) {
+			  arr.forEach(function(item) {
+          var itemDepth=calculateDepth(item);
+				  subArraysLenghts.push(1 + itemDepth);
+				});
+		  } else {
+			  subArraysLenghts.push(1);
+		  }
+	  } else {
+		subArraysLenghts.push(0);
+	  }			
+	
+	  return Math.max(...subArraysLenghts);
   }
 };
+
+
+/*function calculateDepth(arr) {
+  if(!Array.isArray(arr)) {
+    return 'Error!';
+  }
+
+  let depth = 1;
+  arr.forEach(function(item) {
+      let depthCounter = findArrayDepth(item, []);
+      depth < (depthCounter + 2) ? depth = (depthCounter + 2) : depth;
+  });
+  return depth;
+
+  function findArrayDepth(array, depthItem) {
+    array.map(function(item) {
+      if(Array.isArray(item))
+      {
+        depthItem.push(1);
+        if(item.length > 1)
+        {
+            item = item.flat(depthItem.length);
+            console.log(item);
+        }
+        return findArrayDepth(item, depthItem);
+      }
+    });
+    return depthItem.length;
+  }*/
