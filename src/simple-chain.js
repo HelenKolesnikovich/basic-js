850fd355e81b89c276fd6e8c7919ff27edec8dc0
HelenKolesnikovich <solnechnaya_88@list.ru>
2021-03-26 13:56:33 +0300

    fix: add check for the presence of arguments[0] in the 'addLink' method

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -3,35 +3,38 @@ const CustomError = require("../extensions/custom-error");
 const chainMaker = {
   chainArray : [],
   getLength(chain) {
-      return this.chainArray.length;    
+    return this.chainArray.length;    
   },
-
+  
   addLink(value) {
-      value = typeof value === 'string' ? value : String(value);
-      this.chainArray.push(value);
-  return this;
+    if(arguments[0] === undefined)
+    {
+      value = ' ';
+    }
+    value = typeof value === 'string' ? value : String(value);
+    this.chainArray.push(value);
+    return this;
   },
-
+  
   removeLink(position) {
+    if(typeof position !== 'number' || (position < 1 && position > chainArray.length - 1) || !Number.isInteger(position))
+    {
+        return 'Error';
+    }
 
-      if(typeof position !== 'number' || (position < 1 && position > chainArray.length - 1) || !Number.isInteger(position))
-      {
-          return 'Error';
-      }
-
-      this.chainArray.splice(position - 1, 1);
-  return this;
+    this.chainArray.splice(position - 1, 1);
+    return this;
   },
-
+  
   reverseChain() {
-      this.chainArray = this.chainArray.reverse();
-  return this;
+    this.chainArray = this.chainArray.reverse();
+    return this;
   },
-
+  
   finishChain() {
-      let str = this.chainArray.join(' )~~( ');
-      let resultStr = '( '.concat(str, ' )');
-      return resultStr;
+    let str = this.chainArray.join(' )~~( ');
+    let resultStr = '( '.concat(str, ' )');
+    return resultStr;
   }
 };
 
